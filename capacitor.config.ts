e02072d5da15250ea8e109f09eee0fa2f304d125
medmor <elmadioune@gmail.com>
2026-04-03 17:11:25 +0100

    feat: Update AndroidManifest and add network security configuration; create build.gradle and settings.gradle for Capacitor plugins

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'zmclient',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
